a99fe7211ff1e056e8852402cbb8d2616ba8aa86
Reto Kaufmann <reto.kaufmann@adon.li>
2017-04-18 21:13:19 +0200

    listen add funktioniert nun

--- self-v1/controller.js
+++ self-v1/controller.js
@@ -4,12 +4,29 @@ export default class {
 
 
     // $ ist nur eine Namenskonvention fuer DOM Knoten
-    constructor($doc){
+    constructor($doc) {
         console.log("running controller.js - constructor")
 
-        $doc.addEventListener("click", ()=> {
-            console.log(this)
-        })
-
+        this.$doc = $doc
+        // queryselector ist ein CSS selector, deshalb fuer ein ID Feld die #
+        let $input = $doc.querySelector("#new-todo")
+        $input.addEventListener("change", this.onChangeInput.bind(this))
     }
+
+    // ist somit unit-testbar
+    // target ist der ausloesende Event! kommt hier also von $input.addEventListener
+    onChangeInput(ev){
+        console.log("Feldinhalt neu: " +ev.target.value)
+        let $list = this.$doc.querySelector("#list")
+        let $li = this.$doc.createElement("li")
+        $li.innerText = ev.target.value;
+        $list.appendChild($li)
+    }
+
+    /*
+    $doc.addEventListener("click", ()=> {
+        console.log(this)
+    })
+    */
+
 }
